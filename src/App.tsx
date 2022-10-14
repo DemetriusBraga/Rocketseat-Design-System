d4d0a2f6870001837ccf7b932c1183adb9167c61
Demetrius Braga <demetrius.braga@gmail.com>
2022-10-14 16:13:30 -0300

    adicionado addon accessibility e Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
+import { Logo } from './Logo';
 import './styles/global.css';
 
 export function App() {
     return (
-        <div className="m-2">
-            <h1 className="text-2xl font-bold text-gray-800">Hello World</h1>
-
-            <button className="bg-cyan-500 font-medium border rounded px-4 py-1 text-gray-800 hover:bg-cyan-300">
-                Enviar
-            </button>
+        <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+            <Logo />
         </div>
     );
 }
