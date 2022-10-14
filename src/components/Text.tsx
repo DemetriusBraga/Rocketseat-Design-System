897db1efa970c12150898d71922eb505c6698c2f
Demetrius Braga <demetrius.braga@gmail.com>
2022-10-14 11:59:12 -0300

    componentes Text e Text.stories criados

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,7 +1,27 @@
-export function Text() {
+import { ReactNode } from 'react';
+import clsx from 'clsx';
+import { Slot } from '@radix-ui/react-slot';
+
+export interface TextProps {
+    size?: 'sm' | 'md' | 'lg';
+    children: ReactNode;
+    asChild?: Boolean;
+}
+
+export function Text({ size = 'md', children, asChild }: TextProps) {
+    const Comp = asChild ? Slot : 'span';
+
     return (
         <div>
-            <h1>Hello World</h1>
+            <Comp
+                className={clsx('text-gray-100 font-sans', {
+                    'text-xs': size === 'sm',
+                    'text-sm': size === 'md',
+                    'text-md': size === 'lg',
+                })}
+            >
+                {children}
+            </Comp>
         </div>
     );
 }
